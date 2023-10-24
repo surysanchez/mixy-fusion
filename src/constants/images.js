10c046f4efa421c06915df92f63d106f8d06d89d
sury <surysanchezsales@gmail.com>
2023-10-24 14:41:46 -0400

    wip about us section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,7 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
+import MF from '../assets/MF.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -26,6 +27,7 @@ export default {
   bg,
   chef,
   G,
+  MF,
   gallery01,
   gallery02,
   gallery03,
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 import './AboutUs.css';
+import { images } from '../../constants';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.MF} alt="mf letter" />
+    </div>
+    <div className='app__aboutus-content flex__center'>
+      <div className='app__aboutus-content_about'>
+      <h1 className="headtext__cormorant">About Us</h1>
+      <img src={images.fork} style={{width: '130px' ,height: '90px', marginTop: '-30px', marginBottom: '-20px'}} alt="fork" className='fork__img'/>
+      <p className='p__opensans'></p>
+      <button type="button" className="custom__button">Know More</button>
+      </div>
+
+    </div>
   </div>
 );
 
