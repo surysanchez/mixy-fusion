9e06bc185c875e55996df01ed5698cd15e2e67f1
sury <surysanchezsales@gmail.com>
2023-10-30 22:35:16 -0400

    chef component set up

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
 import './Chef.css';
+import { images } from '../../constants';
+
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img  app__wrapper_img-reverse'>
+     <img src={images.chef} alt="chef" />
+    </div>
   </div>
 );
 
